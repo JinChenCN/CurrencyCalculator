718a029aab648aec30b0aa926e7852e825f6a446
cjjjcj4 <2468717+cjjjcj4@users.noreply.github.com>
2017-10-22 23:24:30 +1300

    Update page and js file. Add a new 'Show Historical Rate' button in the first part to show history if you don't want to do the currency conversion, remove 'Check' button from the second part.

--- WebContent/app/controllers/myControllers.js
+++ WebContent/app/controllers/myControllers.js
@@ -1,4 +1,4 @@
-myApp.controller('myCtrl', ['$scope','$http','$filter','myFactory',function ($scope,$http,$filter,myFactory) {  
+myApp.controller('myCtrl', ['$scope','$http','$filter','myFactory', function($scope,$http,$filter,myFactory) {  
 	
     $scope.records = [];
     
@@ -6,57 +6,60 @@ myApp.controller('myCtrl', ['$scope','$http','$filter','myFactory',function ($sc
 	$scope.currencies = ["NZD","USD","EUR","CNY","JPY"];
 	
 	// Reset new data
-    $scope.reset = function () {
+    $scope.reset = function() {
       $scope.newSourceCurrency = $scope.currencies[0];
       $scope.newTargetCurrency = $scope.currencies[1];
       $scope.newInputAmount = 0;
       $scope.newCurrencyRate = 0;
       $scope.newOutputAmount = 0;
-      $scope.newHistoricalData = null;
+      $scope.historicalSourceCurrency = null;
+      $scope.historicalTargetCurrency = null;
+      $scope.labels = null;
+      $scope.data = null;
     }
     
     $scope.reset();
     
-    // Check historical data
-    $scope.check = function (index) {
-      myFactory.setAge(20);	
-      console.log(myFactory.getAge());
-    	
-      var historicalData = $scope.records[index].historicalData;
-      $scope.historicalSourceCurrency = $scope.records[index].sourceCurrency;
-      $scope.historicalTargetCurrency = $scope.records[index].targetCurrency;
-      var dates = new Array();
-      var rates = new Array();
-      angular.forEach(historicalData, function(data){
-    	dates.push($filter('date')(new Date(data.PointInTime),'yyyy-MM-dd'));
-    	rates.push(data.InterbankRate);
+    // Get historical data
+    $scope.getHistoricalData = function() {  
+      if ($scope.newSourceCurrency == $scope.newTargetCurrency)
+        return;
+      myFactory.getData($scope.newSourceCurrency,$scope.newTargetCurrency).then(function(data) {
+        $scope.newCurrencyRate = data.CurrentInterbankRate;
+	    $scope.historicalSourceCurrency = $scope.newSourceCurrency;
+        $scope.historicalTargetCurrency = $scope.newTargetCurrency;
+	    var dates = new Array();
+        var rates = new Array();
+        angular.forEach(data.HistoricalPoints, function(data) {
+    	  dates.push($filter('date')(new Date(data.PointInTime),'yyyy-MM-dd'));
+    	  rates.push(data.InterbankRate);
+        });
+        $scope.labels = dates;
+        $scope.data = new Array();
+        $scope.data.push(rates);
+      }, function(status) {
+        console.log(status);
       });
-      $scope.labels = dates;
-      $scope.data = new Array();
-      $scope.data.push(rates);
-    };
+    }
     
     // Add new data
-    $scope.calculate = function () {
-    	if (!$scope.newInputAmount)
-          return;
-        if ($scope.newSourceCurrency == $scope.newTargetCurrency)
-          return;
-        $http.get('https://api.ofx.com/PublicSite.ApiService/SpotRateHistory/week/'+$scope.newSourceCurrency+'/'+$scope.newTargetCurrency+'?DecimalPlaces=6&ReportingInterval=daily')
-		  .then(function(res) {
-		    if (res.data.CurrentInterbankRate!=null && angular.isNumber(res.data.CurrentInterbankRate)) {
-			  $scope.newCurrencyRate = res.data.CurrentInterbankRate;
-			  $scope.newOutputAmount = $scope.newCurrencyRate*$scope.newInputAmount;
-			  $scope.newHistoricalData = res.data.HistoricalPoints;
-			  $scope.records.push({
-			    sourceCurrency: $scope.newSourceCurrency,
-        	    targetCurrency: $scope.newTargetCurrency,
-        	    inputAmount: $scope.newInputAmount,
-        	    currencyRate: $scope.newCurrencyRate,
-        	    outputAmount: $scope.newOutputAmount,
-        	    historicalData: $scope.newHistoricalData
-    		  });
-			}
-		  });
+    $scope.calculate = function() {
+	  if (!$scope.newInputAmount)
+		return;
+      if ($scope.newSourceCurrency == $scope.newTargetCurrency)
+        return;
+      myFactory.getData($scope.newSourceCurrency,$scope.newTargetCurrency).then(function(data) {
+        if (data.CurrentInterbankRate != null && angular.isNumber(data.CurrentInterbankRate)) {
+  		  $scope.newCurrencyRate = data.CurrentInterbankRate;
+  		  $scope.newOutputAmount = $scope.newCurrencyRate*$scope.newInputAmount;
+  		  $scope.records.push({
+  			sourceCurrency: $scope.newSourceCurrency,
+          	targetCurrency: $scope.newTargetCurrency,
+          	inputAmount: $scope.newInputAmount,
+          	currencyRate: $scope.newCurrencyRate,
+          	outputAmount: $scope.newOutputAmount,
+      	  });
+        }  
+      });
     }
 }]);
--- WebContent/app/services/myServices.js
+++ WebContent/app/services/myServices.js
@@ -1,13 +1,15 @@
-myApp.factory('myFactory', function() {
-        var service = {};//定义一个Object对象'
-        service.name = "张三";
-        var age;//定义一个私有化的变量
-        //对私有属性写getter和setter方法
-        service.setAge = function(newAge){
-            age = newAge;
-        }
-        service.getAge = function(){
-            return age; 
-        }
-        return service;//返回这个Object对象
-    });
+myApp.factory('myFactory', ['$http','$q',function($http,$q) {
+	return {
+	  getData: function(newSourceCurrency,newTargetCurrency) {
+		var deferred = $q.defer();
+		$http.get('https://api.ofx.com/PublicSite.ApiService/SpotRateHistory/week/'+newSourceCurrency+'/'+newTargetCurrency+'?DecimalPlaces=6&ReportingInterval=daily')
+	      .success(function(data, status, headers, config) {
+	        deferred.resolve(data);
+	      })
+	      .error(function(data, status, headers, config) {
+	        deferred.reject(status);
+	      });
+		return deferred.promise;
+	  }
+	}
+}])
